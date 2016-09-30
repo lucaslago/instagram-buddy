49f1223d14d5e7fc9ce53f843b4b9c8647aa7d85
Lucas Lago <lucasbenderlago@gmail.com>
2016-09-30 19:12:41 -0300

    Show only media data

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const parse = require('./parser');
 app.get('/scrape/:hashtag', (req, res) => {
   const url = `https://www.instagram.com/explore/tags/${req.params.hashtag}`;
   rp(url)
-    .then(html => res.json(parse(html)))
+    .then(html => res.json(utils.extractTagData(parse(html))))
     .catch(err => console.log(err));
 });
 
