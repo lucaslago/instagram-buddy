8dbba1be87ece664c55b740d76e1739edcce254c
Lucas Lago <lucasbenderlago@gmail.com>
2016-10-04 11:03:37 -0300

    Enable CORS for all requests

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const utils = require('./utils');
 const rp = require('request-promise');
 const parse = require('./parser');
 
-app.get('/:hashtag', (req, res, next) => {
+app.get('/:hashtag', cors(),  (req, res, next) => {
   const url = `https://www.instagram.com/explore/tags/${req.params.hashtag}`;
   rp(url)
     .then(html => res.json(utils.extractTagData(parse(html))))
